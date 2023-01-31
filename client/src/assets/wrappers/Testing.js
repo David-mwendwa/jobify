c77e3130e05d2fdc92007918ab65ff3bb07ff98d
techdave <klatzdavie19@gmail.com>
2023-01-31 08:06:51 +0300

    add a landing page and setup wrappers

--- client/src/assets/wrappers/Testing.js
+++ client/src/assets/wrappers/Testing.js
@@ -1,11 +1,10 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Wrapper = styled.main`
   nav {
     width: var(--fluid-width);
-    max-width: var(--max-width);
+    width: var(--max-width);
     margin: 0 auto;
-    height: var(--nav-height);
     display: flex;
     align-items: center;
   }
@@ -36,5 +35,5 @@ const Wrapper = styled.main`
       display: block;
     }
   }
-`
-export default Wrapper
+`;
+export default Wrapper;
